2fa4ea7125f498948b654cc8a0d3914f196ea2c4
jppgomes <joaok8@gmail.com>
2018-03-27 19:52:57 -0300

    [UPDATE] implement method get

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from users import views
-from rest_framework import viewsets
+
 
 app_name = 'users'
 
 urlpatterns = [
     path('create/', views.CreateUser.as_view(), name='create'),
+    path('index/', views.create_user, name='index'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,26 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
-from rest_framework import viewsets, generics
+from rest_framework import generics
 from users.serializers import UserSerializer
- 
+from django.contrib.auth.models import User
 
 
 def create_user(request):
-    if resquest.method == 'GET':
-        users = User.object.all()
-        serializers = UserSerializer(users, many=True)
-        return JsonResponse(serializers.data, status=200, safe=False)
-    if resquest.method == 'POST':
+
+    if request.method == 'POST':
         data = JSONParser().parse(request)
-        serializers = UserSerializer(data = data)
+        serializers = UserSerializer(data=data)
         if serializers.is_valid():
             serializers.save
             return JsonResponse(serializers.data, status=201)
-        else:
-            return JsonResponse(serializers.data, status=400)
+        return JsonResponse(serializers.data, status=400)
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializers = UserSerializer(users, many=True)
+        return JsonResponse(serializers.data, status=200, safe=False)
+
 
 class CreateUser(generics.CreateAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
