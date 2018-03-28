48c72cb605e0e486fcd9da5487cbb65db9d33905
jppgomes <joaok8@gmail.com>
2018-03-28 13:27:04 -0300

    [UPDATE] user password  encryption. Co-authored-by: Yoshida-Eduardo <edry97@hotmail.com> Co-authored-by: maxlobo <maxhb.df@gmail.com>

--- users/serializers.py
+++ users/serializers.py
@@ -1,19 +1,30 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('pk', 'username', 'email', 'password')
-    #     extra_kwargs = {'password': {'write_only': True}}
-    #
-    # def create(self, validated_data):
-    #     user = User(
-    #         pk =
-    #         email=validated_data['email'],
-    #         username=validated_data['username']
-    #     )
-    #     user.set_password(validated_data['password'])
-    #     user.save()
-    #     return user
+    #    extra_kwargs = {'password': {'write_only': True}}
+        #extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=make_password(validated_data['password'])
+        )
+        user.save()
+        return user
+
+
+        # def create(self, validated_data):
+        #     user = User(
+        #         email=validated_data['email'],
+        #         username=validated_data['username'],
+        #         password=make_password(validated_data['password'])
+        #     )
+        #     user.save()
+        #     return user
