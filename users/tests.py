e363549cdbb0aa48f30fd776c3fb60ae4fa6342b
jppgomes <joaok8@gmail.com>
2018-03-29 12:57:43 -0300

    [UPDATE] test post method

--- users/tests.py
+++ users/tests.py
@@ -17,8 +17,38 @@ def test_list_user_return_list_user(client):
     users_create = User.objects.create(username='username',
                                        email='email', password='password')
 
-    response = client.get(url, content_type="application/json")
+    response = client.get(url)
 
     assert response.data
     assert isinstance(response.data, list)
     assert response.data[0]['username'] == users_create.username
+
+
+def test_post_user_is_valid_return_201(client):
+    """ Test valid input create user """
+    url = reverse('users:users')
+    data = {'username': "username", 'email': "email@email.com",
+            'password': "password"}
+    response = client.post(url, data=data)
+
+    assert response.status_code == 201
+
+
+def test_post_user_is_not_valid_return_400(client):
+    """ Test not valid input create user """
+    url = reverse('users:users')
+    data = {'username': "", 'email': "", 'password': '12345678910'}
+    response = client.post(url, data=data)
+
+    assert response.status_code == 400
+
+
+def test_post_user_persist_db(client):
+    """ Test persist users in database """
+    url = reverse('users:users')
+    data = {'username': "username", 'email': "email@email.com",
+            'password': "password"}
+    client.post(url, data=data)
+    users = User.objects.all()
+
+    assert users.count() == 1
