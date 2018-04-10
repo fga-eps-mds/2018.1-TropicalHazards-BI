471b74c04d703a43c683224cc542d25819159f58
jppgomes <joaok8@gmail.com>
2018-04-09 21:47:11 -0300

    [UPDATE] refactoring settings and tests components
    Co-authored-by: andrebargas <andrebargas@gmail.com>

--- TropicalHazards_BI/settings.py
+++ TropicalHazards_BI/settings.py
@@ -102,24 +102,20 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth\
-        .password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth\
-        .password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth\
-        .password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth\
-        .password_validation.NumericPasswordValidator',
-    },
+ {
+   'NAME':
+   'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+ },
+ {
+   'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+ },
+ {
+   'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+ },
+ {
+   'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+ },
 ]
-
 REST_AUTH_SERIALIZERS = {
    'USER_DETAILS_SERIALIZER': 'users.serializers.UserDetailsSerializer'
 }
