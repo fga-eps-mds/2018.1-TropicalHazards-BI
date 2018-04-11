64e8a8c8d83858c69eede2163d26f84ecf95a21b
Yoshida-Eduardo <edry97@hotmail.com>
2018-04-11 13:06:54 -0300

    [TEST] added rest-auth login tests
    Co-authored-by: João Pedro <joaok8@gmail.com>
    Co-authored-by: Max Henrique <maxhb.df@gmail.com>

--- TropicalHazards_BI/urls.py
+++ TropicalHazards_BI/urls.py
@@ -20,7 +20,7 @@ from rest_framework_jwt.views import refresh_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls'), name='users'),
-    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/', include('rest_auth.urls'), name='rest-auth'),
     path('refresh-token/', refresh_jwt_token),
     path('projects/', include('projects.urls'), name='projects')
 ]
