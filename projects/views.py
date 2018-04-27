b2b455f6b4d6cc565a91881d0f32a031b6e9b1a4
Yoshida-Eduardo <edry97@hotmail.com>
2018-04-27 16:16:39 -0300

    [FIX] fixed tests

--- projects/views.py
+++ projects/views.py
@@ -28,8 +28,8 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data,
-                        status=status.HTTP_201_CREATED)
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
