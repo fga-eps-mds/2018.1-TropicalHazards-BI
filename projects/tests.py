ccc05dc2b5dd41ccee3955e08f29153eb53c52b3
Pedro Daniel <pedrodaniel.unb@gmail.com>
2018-05-18 12:25:56 -0300

    [ADD] Adding fixture create_project in projects

--- projects/tests.py
+++ projects/tests.py
@@ -14,7 +14,16 @@ def create_user(client):
                                email='email', is_staff=True)
     user.set_password('password')
     user.save()
+    client.login(username='username', password='password')
     return user
+
+
+@pytest.fixture
+def create_project(client, create_user):
+    user = create_user
+    project = mommy.make('Project', user=user)
+    url = reverse('projects:projects-detail', kwargs={'pk': project.id})
+    return project, url
 
 
 def test_get_project_return_200(client):
@@ -37,7 +46,6 @@ def test_list_project_return_list_project(client):
 def test_post_project_is_valid_return_201(client, create_user):
     url = reverse('projects:projects')
     user = create_user
-    client.login(username='username', password='password')
     data = {'name': "nameproject", 'description': "description",
             'user': user.id, "tags": []}
 
@@ -49,8 +57,6 @@ def test_post_project_is_valid_return_201(client, create_user):
 
 def test_post_project_is_not_valid_return_400(client, create_user):
     url = reverse('projects:projects')
-    user = create_user
-    client.login(username='username', password='password')
     data = {'description': "description"}
     json_data = json.dumps(data)
     response = client.post(url, data=json_data,
@@ -64,7 +70,6 @@ def test_post_project_persist_db(client, create_user):
     user = create_user
     data = {'name': "nameproject", 'description': "description",
             'user': user.id, "tags": []}
-    client.login(username='username', password='password')
     json_data = json.dumps(data)
     response = client.post(url, data=json_data,
                            content_type="application/json")
@@ -73,11 +78,8 @@ def test_post_project_persist_db(client, create_user):
     assert projects.count() == 1
 
 
-def test_get_project_detail_return_200(client, create_user):
-    user = create_user
-    client.login(username='username', password='password')
-    project = mommy.make('Project', user=user)
-    url = reverse('projects:projects-detail', kwargs={'pk': project.id})
+def test_get_project_detail_return_200(client, create_project):
+    project, url = create_project
     response = client.get(url)
 
     assert response.status_code == 200
@@ -85,20 +87,15 @@ def test_get_project_detail_return_200(client, create_user):
 
 
 def test_get_project_detail_return_404(client, create_user):
-    user = create_user
-    client.login(username='username', password='password')
-
     url = reverse('projects:projects-detail', kwargs={'pk': 1})
     response = client.get(url)
 
     assert response.status_code == 404
 
 
-def test_put_project_detail_return_200(client, create_user):
+def test_put_project_detail_return_200(client, create_user, create_project):
     user = create_user
-    client.login(username='username', password='password')
-    project = mommy.make('Project', user=user)
-    url = reverse('projects:projects-detail', kwargs={'pk': project.id})
+    project, url = create_project
     data = {'name': "nameproject", 'description': "description",
             'user': user.id}
     json_data = json.dumps(data)
@@ -106,11 +103,9 @@ def test_put_project_detail_return_200(client, create_user):
     assert response.status_code == 200
 
 
-def test_put_project_detail_return400(client, create_user):
+def test_put_project_detail_return400(client, create_user, create_project):
     user = create_user
-    client.login(username='username', password='password')
-    project = mommy.make('Project', user=user)
-    url = reverse('projects:projects-detail', kwargs={'pk': project.id})
+    project, url = create_project
     data = {'name': "", 'description': "description", 'user': user.id}
     json_data = json.dumps(data)
     response = client.put(url, data=json_data, content_type='application/json')
@@ -118,11 +113,8 @@ def test_put_project_detail_return400(client, create_user):
     assert response.status_code == 400
 
 
-def test_delete_project_detail_return_204(client, create_user):
-    user = create_user
-    client.login(username='username', password='password')
-    project = mommy.make('Project', user=user)
-    url = reverse('projects:projects-detail', kwargs={'pk': project.id})
+def test_delete_project_detail_return_204(client, create_user, create_project):
+    project, url = create_project
     response = client.delete(url, content_type='application/json')
 
     assert response.status_code == 204
