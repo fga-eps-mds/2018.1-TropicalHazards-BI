85124fec34f72329fc1b693c39f1517fe521ba5f
andrebargas <andrebargas@gmail.com>
2018-05-24 21:40:20 -0300

    [UPDATE] Making login in metabase to create new question
    Co-authored-by: joao <joaok8@gmail.com>
    Co-authored-by: matheus <matheusbattista@hotmail.com>
    Co-authored-by: max <maxhb.df@gmail.com>

--- metabase/urls.py
+++ metabase/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'iframes'
 
 urlpatterns = [
-    path('<int:pk>/', views.DashboardIframes.as_view(),
+    path("", views.DashboardIframes.as_view(),
          name='dashboard-iframes'),
 ]
--- metabase/views.py
+++ metabase/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+import requests
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import permission_classes
+from rest_framework import permissions
+from rest_framework import status
 
-# Create your views here.
+
+@permission_classes((permissions.AllowAny,))
+class DashboardIframes(APIView):
+
+    def post(self, request, format=None):
+        # name = request.data['name']
+        # display_type = request.data['display_type']
+        # fields = request.data['fields']
+        # data_base_id = 2
+
+        r = requests.post("http://metabase:3000/api/session",
+                          headers={"Content-Type": "application/json"},
+                          json={"username": "andrebargas@gmail.com",
+                                "password": "1234baba"})
+        print(r.json())
+
+        return Response(status=status.HTTP_200_OK)
