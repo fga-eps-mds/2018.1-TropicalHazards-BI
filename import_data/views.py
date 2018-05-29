b8cf1887672e7b7980b0b820232c36bfc6eee0b0
matheusbsilva <matheusbattista@hotmail.com>
2018-05-29 16:06:02 -0300

    [FIX] fix import data feature

--- import_data/views.py
+++ import_data/views.py
@@ -3,6 +3,7 @@ import pymongo
 import json
 import os
 from django.core.files.storage import default_storage
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
@@ -10,6 +11,8 @@ from rest_framework.decorators import permission_classes
 from rest_framework import permissions
 from import_data.serializers import ImportDataSerializer
 from rest_framework import status
+
+from projects.models import Project
 
 
 @permission_classes((permissions.AllowAny,))
@@ -20,19 +23,20 @@ class FileUploadView(APIView):
 
     def post(self, request, format=None):
         file_obj = request.data['file']
-        project_id = request.data['project']
+        project_id = int(request.data['project'])
 
         headersList = json.loads(request.data['headers'])
 
-        serializer = ImportDataSerializer(data=request.data)
+        serializer = ImportDataSerializer(data={'project': project_id})
         if serializer.is_valid():
             file_path = '/code/tmp/' + file_obj.name
             if not file_obj.name.endswith('.csv'):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
             else:
-                with default_storage.open(file_path, 'wb+') as dest:
+                with open(file_path, 'wb') as dest:
                     for chunk in file_obj.chunks():
                         dest.write(chunk)
+                    dest.close()
 
                     dataframe = pandas.read_csv(file_path, header=0)
                     # true_values=headersList['true'],
@@ -70,7 +74,7 @@ class FileUploadView(APIView):
 
                 mongo_client = pymongo.MongoClient('mongo', 27017)
                 mongo_db = mongo_client['main_db']
-                collection = mongo_db['collection_' + project_id]
+                collection = mongo_db['collection_' + str(project_id)]
                 collection.insert(json_data)
                 serializer.save()
                 os.remove(file_path)
