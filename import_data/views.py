0beb451ee49dd24bb5f93e3173efd784dfb46754
matheusbsilva <matheusbattista@hotmail.com>
2018-05-30 18:11:54 -0300

    [UPDATE] create connect_mongo and save_file method on import_data
    Co-authored-by: JP <joaok8@gmail.com>
    Co-authored-by: bargas <andrebargas@gmail.com>
    Co-authored-by: Yoshida <edry97@hotmail.com>
    Co-authored-by: Max <maxhb.df@gmail.com>
    Co-authored-by: Baea <pedrodaniel.unb@gmail.com>

--- import_data/views.py
+++ import_data/views.py
@@ -2,24 +2,50 @@ import pandas
 import pymongo
 import json
 import os
-from django.core.files.storage import default_storage
-from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.decorators import permission_classes
 from rest_framework import permissions
-from import_data.serializers import ImportDataSerializer
 from rest_framework import status
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-from projects.models import Project
+from import_data.serializers import ImportDataSerializer
 
 
-@permission_classes((permissions.AllowAny,))
+def connect_mongo(engine=pymongo, host='mongo', port=27017):
+    mongo_client = engine.MongoClient(host, port)
+    mongo_db = mongo_client['main_db']
+
+    return mongo_db
+
+
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 class FileUploadView(APIView):
     parser_classes = (MultiPartParser, FormParser)
+    mongo_db = connect_mongo()
 
-    # authentication_classes = (JSONWebTokenAuthentication, )
+    authentication_classes = (JSONWebTokenAuthentication, )
+
+    def save_file_tmp(self, file, file_path):
+        with open(file_path, 'wb') as dest:
+            for chunk in file.chunks():
+                dest.write(chunk)
+            dest.close()
+
+    def save_on_mongo(self, data, project_id):
+        """
+        Return True when saved False when not.
+        Any error on data format will return False
+        """
+        try:
+            collection = self.mongo_db['collection_' + str(project_id)]
+            result = collection.insert_many(data)
+            result = result.acknowledged
+        except TypeError:
+            result = False
+        return result
 
     def post(self, request, format=None):
         file_obj = request.data['file']
@@ -29,53 +55,45 @@ class FileUploadView(APIView):
 
         serializer = ImportDataSerializer(data={'project': project_id})
         if serializer.is_valid():
-            file_path = '/code/tmp/' + file_obj.name
             if not file_obj.name.endswith('.csv'):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
             else:
-                with open(file_path, 'wb') as dest:
-                    for chunk in file_obj.chunks():
-                        dest.write(chunk)
-                    dest.close()
+                file_path = '/code/tmp/' + file_obj.name
+                self.save_file_tmp(file_obj, file_path)
 
-                    dataframe = pandas.read_csv(file_path, header=0)
-                    # true_values=headersList['true'],
-                    # false_values=headersList['false'])
+                dataframe = pandas.read_csv(file_path, header=0)
 
-                    for header in headersList:
-                        if header['selected'] is False:
-                            dataframe = dataframe.drop(header['name'], axis=1)
-                        else:
-                            if header['type'] == 'bool':
-                                dataframe[header['name']] =\
-                                    dataframe[header['name']].\
-                                    replace(to_replace=header['true'],
-                                            value=True)
-                                dataframe[header['name']] =\
-                                    dataframe[header['name']].\
-                                    replace(to_replace=header['false'],
-                                            value=False)
-                            try:
-                                dataframe[header['name']] =\
-                                    dataframe[header['name']].\
-                                    astype(header['type'])
-                            except ValueError:
-                                return Response(serializer.errors,
-                                                status=status.
-                                                HTTP_400_BAD_REQUEST)
-                            if header['transform'] == 'upper':
-                                dataframe[header['name']] =\
-                                    dataframe[header['name']].str.upper()
-                            elif header['transform'] == 'lower':
-                                dataframe[header['name']] =\
-                                    dataframe[header['name']].str.lower()
+                for header in headersList:
+                    if header['selected'] is False:
+                        dataframe = dataframe.drop(header['name'], axis=1)
+                    else:
+                        if header['type'] == 'bool':
+                            dataframe[header['name']] =\
+                                dataframe[header['name']].\
+                                replace(to_replace=header['true'],
+                                        value=True)
+                            dataframe[header['name']] =\
+                                dataframe[header['name']].\
+                                replace(to_replace=header['false'],
+                                        value=False)
+                        try:
+                            dataframe[header['name']] =\
+                                dataframe[header['name']].\
+                                astype(header['type'])
+                        except ValueError:
+                            return Response(serializer.errors,
+                                            status=status.
+                                            HTTP_400_BAD_REQUEST)
+                        if header['transform'] == 'upper':
+                            dataframe[header['name']] =\
+                                dataframe[header['name']].str.upper()
+                        elif header['transform'] == 'lower':
+                            dataframe[header['name']] =\
+                                dataframe[header['name']].str.lower()
 
                 json_data = json.loads(dataframe.to_json(orient="records"))
+                self.save_on_mongo(json_data, project_id)
 
-                mongo_client = pymongo.MongoClient('mongo', 27017)
-                mongo_db = mongo_client['main_db']
-                collection = mongo_db['collection_' + str(project_id)]
-                collection.insert(json_data)
                 serializer.save()
                 os.remove(file_path)
 
